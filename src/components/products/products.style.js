e2288a0953eaad79baf21f7b0518f0267f8a95b1
heyyayesh <biswalayesh3@gmail.com>
2022-05-21 18:23:02 +0530

    Add visual cue to confirm button click

--- src/components/products/products.style.js
+++ src/components/products/products.style.js
@@ -65,12 +65,18 @@ export const AddToCart = styled.button`
   height: 40px;
   font-size: 20px;
   cursor: pointer;
+  transform: scale(1);
+  transition: transform 50ms ease-in-out;
 
   &:hover {
     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.07),
       0 4px 8px rgba(0, 0, 0, 0.07), 0 8px 16px rgba(0, 0, 0, 0.07),
       0 16px 32px rgba(0, 0, 0, 0.07), 0 32px 64px rgba(0, 0, 0, 0.07);
     border-radius: 10px;
+  }
+
+  &:active {
+    transform: scale(0.98);
   }
 `;
 
